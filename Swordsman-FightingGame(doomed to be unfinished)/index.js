9fa045a0ab6aca018cc17661349e847bdf19a6b3
Damon L Thomas <damothom18@bossierschools.org>
2026-03-05 13:28:55 -0600

    Dice App

--- Swordsman-FightingGame(doomed to be unfinished)/index.js
+++ Swordsman-FightingGame(doomed to be unfinished)/index.js
@@ -8,24 +8,23 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 
-const background = new Sprite ({
+const background = new Sprite({
   position: {
-    x:0,
-    y:0,
+    x: 0,
+    y: 0,
   },
-  imageSrc: "./Images/background.png"
+  imageSrc: "./Images/background.png",
+});
 
-})
-
-const shop = new Sprite ({
+const shop = new Sprite({
   position: {
     x: 600,
     y: 128,
   },
   imageSrc: "./Images/shop.png",
   scale: 2.75,
-  framesMax: 6
-})
+  framesMax: 6,
+});
 
 const player = new Fighter({
   position: {
@@ -39,6 +38,51 @@ const player = new Fighter({
   offset: {
     x: 0,
     y: 0,
+  },
+  imageSrc: "./Images/samuraiMack/Idle.png",
+  framesMax: 8,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 157,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./Images/samuraiMack/Idle.png",
+      framesMax: 8,
+    },
+    run: {
+      imageSrc: "./Images/samuraiMack/Run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: "./Images/samuraiMack/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./Images/samuraiMack/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./Images/samuraiMack/Attack1.png",
+      framesMax: 6,
+    },
+    takeHit: {
+      imageSrc: "./Images/samuraiMack/Take Hit - white silhouette.png",
+      framesMax: 4,
+    },
+    death: {
+      imageSrc: "./Images/samuraiMack/Death.png",
+      framesMax: 6,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: 100,
+      y: 50,
+    },
+    width: 160,
+    height: 50,
   },
 });
 
@@ -55,6 +99,51 @@ const enemy = new Fighter({
   offset: {
     x: -50,
     y: 0,
+  },
+  imageSrc: "./Images/kenji/Idle.png",
+  framesMax: 4,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 167,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./Images/kenji/Idle.png",
+      framesMax: 4,
+    },
+    run: {
+      imageSrc: "./Images/kenji/Run.png",
+      framesMax: 8,
+    },
+    jump: {
+      imageSrc: "./Images/kenji/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./Images/kenji/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./Images/kenji/Attack1.png",
+      framesMax: 4,
+    },
+    takeHit: {
+      imageSrc: "./Images/kenji/Take hit.png",
+      framesMax: 3,
+    },
+    death: {
+      imageSrc: "./Images/kenji/Death.png",
+      framesMax: 7,
+    },
+  },
+  attackBox: {
+    offset: {
+      x: -170,
+      y: 50,
+    },
+    width: 170,
+    height: 50,
   },
 });
 
@@ -79,49 +168,76 @@ const keys = {
   },
 };
 
-
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  background.update()
+  background.update();
   shop.update();
   player.update();
   enemy.update();
 
-
   player.velocity.x = 0;
   enemy.velocity.x = 0;
+
   if (keys.a.pressed === true && player.lastKey === "a") {
     player.velocity.x = -5;
+    player.switchSprite("run");
   } else if (keys.d.pressed === true && player.lastKey === "d") {
     player.velocity.x = 5;
+    player.switchSprite("run");
+  } else {
+    player.switchSprite("idle");
+  }
+
+  if (player.velocity.y < 0) {
+    player.switchSprite("jump");
+  } else if (player.velocity.y > 0) {
+    player.switchSprite("fall");
   }
 
   if (keys.ArrowLeft.pressed === true && enemy.lastKey === "ArrowLeft") {
     enemy.velocity.x = -5;
+    enemy.switchSprite("run");
   } else if (
     keys.ArrowRight.pressed === true &&
     enemy.lastKey === "ArrowRight"
   ) {
     enemy.velocity.x = 5;
+    enemy.switchSprite("run");
+  } else {
+    enemy.switchSprite("idle");
+  }
+  if (enemy.velocity.y < 0) {
+    enemy.switchSprite("jump");
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprite("fall");
   }
 
   if (
     rectangularCollision({ rectangle1: player, rectangle2: enemy }) &&
-    player.isAttacking
+    player.isAttacking &&
+    player.framesCurrent === 4
   ) {
+    enemy.takeHit();
     player.isAttacking = false;
-    enemy.health -= 20;
+
     document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+  }
+  if (player.isAttacking && player.framesCurrent === 4) {
+    player.isAttacking = false;
   }
   if (
     rectangularCollision({ rectangle1: enemy, rectangle2: player }) &&
-    enemy.isAttacking
+    enemy.isAttacking &&
+    enemy.framesCurrent === 2
   ) {
+    player.takeHit();
     enemy.isAttacking = false;
-    player.health -= 20;
     document.querySelector("#playerHealth").style.width = player.health + "%";
+  }
+  if (enemy.isAttacking && enemy.framesCurrent === 2) {
+    enemy.isAttacking = false;
   }
 
   if (enemy.health <= 0 || player.health <= 0) {
@@ -130,43 +246,49 @@ function animate() {
   }
 }
 
-
 decreaseTimer();
 
 animate();
 
 window.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "d":
-      keys.d.pressed = true;
-      player.lastKey = "d";
-      break;
-    case "a":
-      keys.a.pressed = true;
-      player.lastKey = "a";
-      break;
-    case "w":
-      player.velocity.y = -20;
-      break;
-    case "ArrowRight":
-      keys.ArrowRight.pressed = true;
-      enemy.lastKey = "ArrowRight";
-      break;
-    case "ArrowLeft":
-      keys.ArrowLeft.pressed = true;
-      enemy.lastKey = "ArrowLeft";
-      break;
-    case "ArrowUp":
-      enemy.velocity.y = -20;
-      break;
-    case " ":
-      player.attack();
-      break;
-    case "ArrowDown":
-      enemy.attack();
-      break;
+  if (!player.dead) {
+    switch (event.key) {
+      case "d":
+        keys.d.pressed = true;
+        player.lastKey = "d";
+        break;
+      case "a":
+        keys.a.pressed = true;
+        player.lastKey = "a";
+        break;
+      case "w":
+        player.velocity.y = -20;
+        break;
+      case " ":
+        player.attack();
+        break;
+    }
   }
-  console.log(event.key);
+  if (!enemy.dead) {
+    switch (event.key) {
+      case "ArrowRight":
+        keys.ArrowRight.pressed = true;
+        enemy.lastKey = "ArrowRight";
+        break;
+      case "ArrowLeft":
+        keys.ArrowLeft.pressed = true;
+        enemy.lastKey = "ArrowLeft";
+        break;
+      case "ArrowUp":
+        enemy.velocity.y = -20;
+        break;
+      case "ArrowDown":
+        enemy.attack();
+        break;
+    }
+  }
+
+  //console.log(event.key);
 });
 window.addEventListener("keyup", (event) => {
   switch (event.key) {
--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,4 +1,110 @@
 $(document).ready(function () {
   // Your code goes here
+  var dieAmount = 1
+  var die2Amount = 1
+  var dieTotal = 2
+  $("<div>")
+    .css("height", 15)
+    .css("width", 15)
+    .css("background-color", "black")
+    .css("position", "absolute")
+    .css("top", 50)
+    .css("left", 50)
+    .appendTo("#die");
+    $("<div>")
+    .css("height", 15)
+    .css("width", 15)
+    .css("background-color", "red")
+    .css("position", "absolute")
+    .css("top", 50)
+    .css("left", 50)
+    .appendTo("#die2");
 
+  function makeDot(top, left, elementID, color){
+    $("<div>")
+    .css("height", 15)
+    .css("width", 15)
+    .css("background-color", color)
+    .css("position", "absolute")
+    .css("top", top)
+    .css("left", left)
+    .appendTo(elementID);
+  }
+
+  function rollDie(dieID, colour){
+    $(dieID).empty();
+    var randomNum = Math.ceil(Math.random() * 6);
+    console.log(randomNum)
+    if (randomNum === 1) {
+      makeDot(50, 50, dieID, colour); // middle middle
+      if (dieID === "#die"){
+        dieAmount = 1
+      } else {
+        die2Amount = 1
+      }
+    } else if (randomNum === 2) {
+      makeDot(25, 25, dieID, colour); // top left
+      makeDot(75, 75, dieID, colour); // bottom right
+      if (dieID === "#die"){
+        dieAmount = 2
+      } else {
+        die2Amount = 2
+      }
+    } else if (randomNum === 3) {
+      makeDot(25, 25, dieID, colour); // top left
+      makeDot(75, 75, dieID, colour); // bottom right
+      makeDot(50, 50, dieID, colour); // middle middle
+      if (dieID === "#die"){
+        dieAmount = 3
+      } else {
+        die2Amount = 3
+      }
+    } else if (randomNum === 4) {
+      makeDot(75, 75, dieID, colour); // bottom right
+      makeDot(25, 25, dieID, colour); // top left
+      makeDot(25, 75, dieID, colour); // bottom left
+      makeDot(75, 25, dieID, colour); // top right
+      if (dieID === "#die"){
+        dieAmount = 4
+      } else {
+        die2Amount = 4
+      }
+    } else if (randomNum === 5) {
+      makeDot(50, 50, dieID, colour); // middle middle
+      makeDot(75, 75, dieID, colour); // bottom right
+      makeDot(25, 25, dieID, colour); // top left
+      makeDot(25, 75, dieID, colour); // bottom left
+      makeDot(75, 25, dieID, colour); // top right
+      if (dieID === "#die"){
+        dieAmount = 5
+      } else {
+        die2Amount = 5
+      }
+    } else if (randomNum === 6) {
+      makeDot(75, 75, dieID, colour); // bottom right
+      makeDot(25, 25, dieID, colour); // top left
+      makeDot(25, 75, dieID, colour); // bottom left
+      makeDot(75, 25, dieID, colour); // top right
+      makeDot(50, 25, dieID, colour);
+      makeDot(50, 75, dieID, colour);
+      if (dieID === "#die"){
+        dieAmount = 6
+      } else {
+        die2Amount = 6
+      }
+    }
+  }
+
+  function handleClick(){
+    rollDie("#die", "black")
+    dieTotal = dieAmount + die2Amount
+    $("p").text("Total: " + dieTotal);
+  }
+  function handleClick2(){
+    rollDie("#die2", "red")
+    dieTotal = dieAmount + die2Amount
+    $("p").text("Total: " + dieTotal);
+  }
+  $("#die").on("click", handleClick);
+  $("#die2").on("click", handleClick2);
 });
